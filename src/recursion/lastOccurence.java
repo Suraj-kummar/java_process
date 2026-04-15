2c3c19238507a5c04f5fe4a7a4616eafcfbbb17d
suraj kumar <surajnsg115@gmail.com>
2026-04-15 08:00:39 +0530

    Add lastOccurence.java - find last occurrence using recursion

---
+++ src/recursion/lastOccurence.java
@@ -0,0 +1,25 @@
+// package recursion;
+
+// public class lastOccurence {
+//     public static int lastOcc (int arr[], int key, int i){
+// if(i==arr.length){
+//     return -1;
+// }
+// int isFound = lastOcc(arr, key, i+1);
+// if(isFound == -1 && arr[] == key){
+//     return isFound;
+// }
+
+// //  chech wirh self
+// if(arr[i] == key){
+//     return  i;
+// }
+// return i;
+
+// }
+    
+//     public static void main(String[] args) {
+//         int  arr[] = {8,3,6,5,10,2,5};
+//         System.out.println(lastOcc(arr, 5, 0));
+//     }  
+// }
