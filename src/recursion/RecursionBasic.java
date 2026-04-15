02f6e9837e30a46ea147474e74f489eab49c6bb3
suraj kumar <surajnsg115@gmail.com>
2026-04-15 08:01:28 +0530

    Add RecursionBasic.java - recursion fundamentals

---
+++ src/recursion/RecursionBasic.java
@@ -0,0 +1,8 @@
+package recursion;
+
+
+// Recursion is a methos of solving a computational problem where the solution depens on solutions to smaller instances of the same problem
+
+//
+public class RecursionBasic {
+}
