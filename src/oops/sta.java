1b020fe86aba2b35b19e72be27ec0e04f9ba6cbb
suraj kumar <surajnsg115@gmail.com>
2026-04-15 07:57:59 +0530

    Add sta.java - static concepts in oops

---
+++ src/oops/sta.java
@@ -0,0 +1,44 @@
+// package oops;
+// //  STATIC KEYWORD :-------  static keywords in java is used to share the same variable or method of a given class..
+// //   proprties, functions, blocks, Nested Classes
+
+// //  SUPER KEYWORDS------  super keyword is used to immediate parent class constructor, method, variable, etc. it is used to refer immediate parent class object.
+// // --- to access parents propeties , --- to access parent's functions , -- to access parent's constructor  
+// public class sta{ 
+//     public static void main(String[] args) {
+//     Horse h =  new Horse();
+        
+//     }
+    
+// }
+
+
+// class studenet {
+//     String name ;
+//     int roll;
+
+//     static  String schoolName;
+
+//     void setName(String name) {
+//         this.name =  name;
+    
+//     }
+//     String getName() {
+//         return  this.name;
+//     }
+// }
+//   class Animal {
+
+//     Animal() { 
+//         System.out.println("animal constructor is called ");
+//     }
+    
+//   }
+
+//   class Horse extends Animal{
+    
+//     Horse() {
+//         super();
+//         System.out.println("horse constructor is called ");
+//     }
+//   }
